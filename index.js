00130a47df30425e21f3a50b841bac5e794a64e0
Blade21Shade <blade21shade@gmail.com>
2025-07-03 14:01:46 -0500

    Some small formatting changes
    style.css
    - .sketch-box
     - Add a border to each box
     - Get rid of height and width definitions; they are moved to the JS for the boxes
    
    index.js
    - Make a heightWidth variable to hold the height and width
     - This can now be used dynamically by setting it to some calculated value based on user input
     - This isn't 100% necessary, but the ability is there if it would be made in the future
    - Add justify content and align items to each flexRow and add a margin on the bottom so they space out some
    - Added a couple comments to show that rows and squares are being made
     - These comments are somewhat redundant, but are small and help space out the logic some
     - If I decide to refactor the code to use function calls instead of just for loops, these also show where to make those function definitions and calls

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 let numOfSquaresPerSide = 16;
+let heightWidth = "60px";
 
 const container = document.querySelector(".sketch-container");
+// Create rows
 for (let i = 0; i < numOfSquaresPerSide; i++) {
     const flexRow = document.createElement("div");
     flexRow.classList.toggle("flexRow");
     flexRow.style.display = "flex";
     flexRow.style.gap = "10px";
+    flexRow.style.justifyContent = "space-evenly";
+    flexRow.style.alignItems = "center";
+    flexRow.style.marginBottom = "10px";
+
+    // Create squares in each row
     for (let j = 0; j < numOfSquaresPerSide; j++) {
         const sketchBox = document.createElement("div");
+        sketchBox.style.height = heightWidth;
+        sketchBox.style.width = heightWidth;
         sketchBox.classList.toggle("sketch-box");
         flexRow.appendChild(sketchBox);
     }
